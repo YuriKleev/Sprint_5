8f07cddc7cd410e4971be16c950f30951e8522f4
Yuri Kleev <logalen@mail.ru>
2025-11-17 19:12:55 +0300

    Add transition to profile tests

---
+++ tests/test_transit_to_profile.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import conftest
+from locators import Locators
+from helper import fill_reg_fields
+
+class TestTransitionToProfileStellarBurgers:
+
+    def test_transit_to_profile(self,driver,registration_user):
+        WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON))
+        email,password=registration_user[1],registration_user[2]
+        fill_reg_fields (driver,email,password)
+        driver.find_element(*Locators.LOGIN_BUTTON).click()
+        WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.PROFILE_BUTTON))
+        driver.find_element(*Locators.PROFILE_BUTTON).click()
+        assert WebDriverWait(driver, 3).until(EC.visibility_of_element_located(Locators.LOGOUT_BUTTON))
