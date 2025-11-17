f7b3f54d3cff8cd31de5dbb2c470b195b90dc498
Yuri Kleev <logalen@mail.ru>
2025-11-17 22:13:03 +0300

    Update locators

--- locators.py
+++ locators.py
@@ -14,16 +14,13 @@ class Locators():
     CONSTRUCTOR_BUTTON=(By.XPATH, '//p[text()="Конструктор"]')     # Кнопка перехода в конструктор
     CREATE_BURGER=(By.XPATH, '//h1[text()="Соберите бургер"]')     # Заголовок "Соберите бургер"
     LOGO_STELLARBURGERS=(By.XPATH, '//div/a[@href="/"]')     # Лого Stellar Burgers
-
     PROFILE_BUTTON=(By.XPATH, '//p[text()="Личный Кабинет"]')     # Кнопка перехода в личный кабинет
     LOGIN_BUTTON=(By.XPATH, '//button[text()="Войти"]')     # Кнопка "Войти" на странице входа
     LOGIN_EMAIL_INPUT=(By.XPATH, "//input[@type='text']")     # Поле ввода логина на странице входа
     LOGIN_PASSWORD_INPUT=(By.XPATH, ".//input[@type='password']")     # Поле ввода пароля на странице входа
     LOGOUT_BUTTON=(By.XPATH, '//button[text()="Выход"]')     # Кнопка "Выход" в Личном Кабинете
-
     ERROR_INCORRECT_PASSWORD=(By.XPATH, '//p[text()="Некорректный пароль"]')     # Сообщение о некорректном пароле
     ERROR_BORDER=(By.CSS_SELECTOR, ".input_status_error")     # Красная рамка вокруг поля ввода пароля
-
     SELECTOR_BUNS=(By.XPATH, '//span[text()="Булки"]/parent::div')     # Раздел с булками в конструкторе
     SELECTOR_SAUCES=(By.XPATH, '//span[text()="Соусы"]/parent::div')     # Раздел с соусами в конструкторе
-    SELECTOR_FILLINGS=(By.XPATH, '//span[text()="Начинки"]/parent::div')     # Раздел с начинками в конструкторе    
+    SELECTOR_FILLINGS=(By.XPATH, '//span[text()="Начинки"]/parent::div')     # Раздел с начинками в конструкторе
